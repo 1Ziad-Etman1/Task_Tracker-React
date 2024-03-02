836df616d911fd5ea3c53c3e87548444f1da3879
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2024-03-02 15:14:30 -0800

    finished the basic functionalities

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import AddTask from "./components/AddTask";
+import React, { useState } from "react";
+import ToDo from "./components/ToDo";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  console.log(taskList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold py-6 pl-6">Task Tracker</h1>
+      <p className="text-xl pl-6">hi there</p>
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6">click</p>
+        <AddTask taskList={taskList} setTaskList={setTaskList} />
+        <p className="text-xl my-2">to add task</p>
+      </div>
+      <div>
+        <h2 className="ml-6 text-xl w-3/4  font-semibold w-max-lg py-2 px-4 bg-gray-300">
+          To Do:
+        </h2>
+        {taskList
+          .slice(0)
+          .reverse()
+          .map((task, i) => (
+            <>
+              <ToDo
+                key={new Date().getTime()}
+                task={task}
+                index={i}
+                taskList={taskList}
+                setTaskList={setTaskList}
+              />
+            </>
+          ))}
+      </div>
+    </>
   );
 }
 
---
+++ task-tracker/src/components/AddTask.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+const AddTask = ({ taskList, setTaskList }) => {
+  const [addModal, setAddModal] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    if (name === "projectName") setProjectName(value);
+    if (name === "taskDescription") setTaskDescription(value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, { projectName, taskDescription }]);
+    setAddModal(false);
+    setProjectName("");
+    setTaskDescription("");
+  };
+
+  return (
+    <>
+      <button
+        className="bg-red-600 text-white uppercase 
+            text-sm font-semibold py-1 mx-1.5 
+            pl-2 pr-2.5 rounded hover:opacity-80"
+        onClick={() => setAddModal(true)}
+      >
+        +New
+      </button>
+      {addModal ? (
+        <>
+          <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-100">
+            <div className="w-9/12 max-w-lg bg-white rounded-lg shadow-md relative flex flex-col">
+              <div className="flex flex-row justify-between p-5 border-b border-slate-200 rounded-t">
+                <h3 className="bg-white text-3xl font-semibold">
+                  Add new Task
+                </h3>
+                <button
+                  className="px-1 text-gray-400 float-right text-xl leading-none font-semibold block"
+                  onClick={() => setAddModal(false)}
+                >
+                  x
+                </button>
+              </div>
+              <form className="px-6 pt-6 pb-4">
+                <div>
+                  <label
+                    className="tracking-wide uppercase text-gray-700 text-xs font-semibold mb-2"
+                    htmlFor="project-name"
+                  >
+                    Project Name
+                  </label>
+                  <input
+                    className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                    id="project-name"
+                    type="text"
+                    placeholder="Project name"
+                    name="projectName"
+                    value={projectName}
+                    onChange={handleInput}
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="task-description"
+                    className="tracking-wide uppercase text-gray-700 text-xs font-semibold mb-2"
+                  >
+                    Task Description
+                  </label>
+                  <textarea
+                    id="task-description"
+                    rows="5"
+                    className="w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-5 leading-tight focus:outline-none focus:bg-white"
+                    placeholder="Task Description"
+                    name="taskDescription"
+                    value={taskDescription}
+                    onChange={handleInput}
+                    required
+                  />
+                </div>
+              </form>
+
+              <div className="flex justify-end p-6 border-t border-slate-200 rounded-b">
+                <button
+                  className="bg-blue-500 text-white font-semibold uppercase text-sm px-6 py-3 ronded hover:opacity-70"
+                  onClick={handleAdd}
+                >
+                  Add Task
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default AddTask;
