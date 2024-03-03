a88aecbe405bf5e7baf1aa57c3cd17facfd18175
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2024-03-02 17:19:59 -0800

    added dnd functionality

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,50 @@
 import logo from "./logo.svg";
 import AddTask from "./components/AddTask";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
-
+import { useDrag } from "react-dnd";
+import { useDrop } from "react-dnd";
 function App() {
   const [taskList, setTaskList] = useState([]);
-  console.log(taskList);
+  const [completed, setCompleted] = useState([]);
+
+  useEffect(() => {
+    let array = localStorage.getItem("taskList");
+
+    if (array) {
+      setTaskList(JSON.parse(array));
+    }
+  }, []);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "todo",
+    drop: (item) =>
+      addToCompleted(
+        item.id,
+        item.projectName,
+        item.taskDescription,
+        item.timestamp,
+        item.duration
+      ),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addToCompleted = (
+    id,
+    projectName,
+    taskDescription,
+    timestamp,
+    duration
+  ) => {
+    const moveTask = taskList.filter((task) => id === task.id);
+    setCompleted((completed) => [
+      ...completed,
+      { moveTask, projectName, taskDescription, timestamp, duration },
+    ]);
+  };
+
   return (
     <>
       <h1 className="text-2xl font-bold py-6 pl-6">Task Tracker</h1>
@@ -15,19 +54,35 @@ function App() {
         <AddTask taskList={taskList} setTaskList={setTaskList} />
         <p className="text-xl my-2">to add task</p>
       </div>
-      <div>
-        <h2 className="ml-6 text-xl w-3/4 max-w-lg font-semibold w-max-lg py-2 px-4 bg-gray-300">
-          To Do:
-        </h2>
-        {taskList.map((task, i) => (
-          <ToDo
-            key={i}
-            task={task}
-            index={i}
-            taskList={taskList}
-            setTaskList={setTaskList}
-          />
-        ))}
+      <div className="flex flex-row">
+        <div className="w-full">
+          <h2 className="ml-6 text-xl w-3/4 max-w-lg font-semibold w-max-lg py-2 px-4 bg-gray-300">
+            To Do:
+          </h2>
+          {taskList.map((task, i) => (
+            <ToDo
+              key={i}
+              task={task}
+              index={i}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+          ))}
+        </div>
+        <div className="w-full flex flex-col" ref={drop}>
+          <h2 className="text-xl w-3/4 max-w-lg font-semibold w-max-lg py-2 px-4 bg-gray-300">
+            Completed:
+          </h2>
+          {completed.map((task, i) => (
+            <ToDo
+              key={i}
+              task={task}
+              index={i}
+              taskList={taskList}
+              setTaskList={setTaskList}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- task-tracker/src/components/ToDo.jsx
+++ task-tracker/src/components/ToDo.jsx
@@ -1,10 +1,23 @@
 import EditTask from "./EditTask";
 
 import { useEffect, useState } from "react";
-
+import { useDrag } from "react-dnd";
 const ToDo = ({ task, index, taskList, setTaskList }) => {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(task.duration);
   const [running, setRunning] = useState(false);
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "todo",
+    item: {
+      id: index,
+      projectName: task.projectName,
+      taskDescription: task.taskDescription,
+      timestamp: task.timestamp,
+      duration: task.duration,
+    },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
 
   useEffect(() => {
     let interval;
@@ -18,15 +31,33 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
     return () => clearInterval(interval);
   }, [running]);
 
+  const handleStop = () => {
+    setRunning(false);
+    let taskIndex = taskList.indexOf(task);
+    taskList.splice(taskIndex, 1, {
+      projectName: task.projectName,
+      taskDescription: task.taskDescription,
+      timestamp: task.timestamp,
+      duration: time,
+    });
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+    window.location.reload();
+  };
+
   const handleDelete = (e) => {
     e.preventDefault();
     let tasks = taskList.filter((t) => t !== task);
+    localStorage.setItem("taskList", JSON.stringify(tasks));
     setTaskList(tasks);
+    window.location.reload();
   };
 
   return (
     <>
-      <div className="flex flex-col flex-wrap items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg">
+      <div
+        className="flex flex-col flex-wrap items-start justify-start bg-white my-4 ml-6 py-4 px-6 w-3/4 max-w-lg"
+        ref={drag}
+      >
         <div className="w-full flex flex-row justify-between">
           <p className="font-semibold text-xl ">{task.projectName}</p>
           <EditTask
@@ -37,8 +68,8 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
           />
         </div>
         <p className="text-lg py-2">{task.taskDescription}</p>
-        <div className="w-full flex sm:flex-col md:flex-row items-center justify-between">
-          <div className="w-1/4 text-xl font-semibold py-4">
+        <div className="w-full flex sm:flex-row sm:justify-center flex-col items-center justify-between">
+          <div className="sm:w-1/4  text-xl font-semibold py-4">
             <span>{("0" + Math.floor((time / 3600000) % 24)).slice(-2)}:</span>
             <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
             <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
@@ -51,9 +82,7 @@ const ToDo = ({ task, index, taskList, setTaskList }) => {
               <>
                 <button
                   className="border rounded-lg py-1 px-3"
-                  onClick={() => {
-                    setRunning(false);
-                  }}
+                  onClick={handleStop}
                 >
                   Stop
                 </button>
